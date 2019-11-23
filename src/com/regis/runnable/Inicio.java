985154331cc41f5175237fe5536d089be33df077
Regis Yamamoto <regisyamaoto@gmail.com>
2019-11-23 14:53:42 -0200

    Primeiro commit

---
+++ src/com/regis/runnable/Inicio.java
@@ -0,0 +1,12 @@
+package com.regis.runnable;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		
+		MinhaThread t1 = new MinhaThread("Thread #1", 600);
+		MinhaThread t2 = new MinhaThread("Thread #2", 900);
+
+	}
+
+}
